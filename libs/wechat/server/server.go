c116a8a7c5cda3939671cc1895a12eda0407d22b
cuirixin <cuirixin@daixiaomi.com>
2018-01-08 21:06:39 +0800

    UP

--- libs/wechat/server/server.go
+++ libs/wechat/server/server.go
@@ -132,7 +132,7 @@ func (srv *Server) getMessage() (interface{}, error) {
 			return nil, fmt.Errorf("消息解密失败, err=%v", err)
 		}
 	} else {
-		rawXMLMsgBytes, err = ioutils.ReadAll(srv.Request.Body)
+		rawXMLMsgBytes, err = ioutil.ReadAll(srv.Request.Body)
 		if err != nil {
 			return nil, fmt.Errorf("从body中解析xml失败, err=%v", err)
 		}
